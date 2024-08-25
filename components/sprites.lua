4fb8d47bfdb9370854c52788ff54ab1350e56791
lpnh <paniguel.lpnh@gmail.com>
2024-08-25 16:29:52 -0300

    feat: animate and set sprite position

--- components/sprites.lua
+++ components/sprites.lua
@@ -4,6 +4,19 @@ local M = {}
 M.x = 48
 M.y = 36
 
+-- sprite animation
+M.current_frame = 1
+M.frame_duration = 1
+M.timer = 0
+
+function M.update_frame(dt)
+  M.timer = M.timer + dt
+  if M.timer > M.frame_duration then
+    M.timer = M.timer - M.frame_duration
+    M.current_frame = 3 - M.current_frame
+  end
+end
+
 function M.load_invaders(img)
   return {
     {
--- main.lua
+++ main.lua
@@ -18,25 +18,28 @@ function love.load()
   ship = sprites.load_ship(IMG)
 end
 
+function love.update(dt)
+  sprites.update_frame(dt)
+end
+
+local margin = (game_screen.width - (11 * (sprites.x + 10))) / 2
+
 function love.draw()
   local time = love.timer.getTime()
-
   -- splash.start(time)
 
   if time >= 0 then
     game_screen.frame()
-    -- TODO: animate each alien
-    --  the animation have only 2 frames [_][1] and [_][2]
-    love.graphics.draw(IMG, invaders[1][1], window.center.x - (sprites.x / 2), game_screen.pos_y0 + sprites.y)
-    love.graphics.draw(IMG, invaders[1][2], window.center.x + (sprites.x / 2), game_screen.pos_y0 + sprites.y)
 
-    love.graphics.draw(IMG, invaders[2][1], window.center.x - (sprites.x / 2), game_screen.pos_y0 + sprites.y * 2)
-    love.graphics.draw(IMG, invaders[2][2], window.center.x + (sprites.x / 2), game_screen.pos_y0 + sprites.y * 2)
-
-    love.graphics.draw(IMG, invaders[3][1], window.center.x - (sprites.x / 2), game_screen.pos_y0 + sprites.y * 3)
-    love.graphics.draw(IMG, invaders[3][2], window.center.x + (sprites.x / 2), game_screen.pos_y0 + sprites.y * 3)
+    for i = 1, 11, 1 do
+      love.graphics.draw(IMG, invaders[1][sprites.current_frame], game_screen.pos_x0 + i * sprites.x + margin, game_screen.pos_y0 + sprites.y * 1)
+      love.graphics.draw(IMG, invaders[2][sprites.current_frame], game_screen.pos_x0 + i * sprites.x + margin, game_screen.pos_y0 + sprites.y * 2.3)
+      love.graphics.draw(IMG, invaders[2][sprites.current_frame], game_screen.pos_x0 + i * sprites.x + margin, game_screen.pos_y0 + sprites.y * 3.5)
+      love.graphics.draw(IMG, invaders[3][sprites.current_frame], game_screen.pos_x0 + i * sprites.x + margin, game_screen.pos_y0 + sprites.y * 4.8)
+      love.graphics.draw(IMG, invaders[3][sprites.current_frame], game_screen.pos_x0 + i * sprites.x + margin, game_screen.pos_y0 + sprites.y * 6.1)
+    end
 
     -- TODO: add ship movement
-    love.graphics.draw(IMG, ship, window.center.x + (sprites.x / 2), game_screen.pos_y1 - sprites.y - 10)
+    love.graphics.draw(IMG, ship, window.center.x - (sprites.x / 2), game_screen.pos_y1 - sprites.y - 10)
   end
 end
