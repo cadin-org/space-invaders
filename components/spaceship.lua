61cfc4d9ccda57ae682de91ba8578a834299ae31
Vinicius Tibo <viniciustibo.vt@gmail.com>
2024-08-26 11:31:15 -0300

    add spaceship moviment limit

--- components/spaceship.lua
+++ components/spaceship.lua
@@ -1,3 +1,5 @@
+local game_screen = require 'libcadin.game-screen'
+
 local Spaceship = {}
 Spaceship.__index = Spaceship
 
@@ -11,9 +13,9 @@ function Spaceship:load(img, x, y)
 end
 
 function Spaceship:move(left, right)
-  if love.keyboard.isDown(left) then
+  if love.keyboard.isDown(left) and self.x - 1 > game_screen.pos_x0 then
     self.x = self.x - self.speed
-  elseif love.keyboard.isDown(right) then
+  elseif love.keyboard.isDown(right) and self.x + 49 < game_screen.pos_x1 then
     self.x = self.x + self.speed
   end
 end
