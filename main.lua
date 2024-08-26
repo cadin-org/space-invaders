fb92d91a021c8902546108c103064b82f7a73244
lpnh <paniguel.lpnh@gmail.com>
2024-08-26 15:09:37 -0300

    feat: implement invaders table and movement

--- main.lua
+++ main.lua
@@ -3,7 +3,15 @@ local game_screen = require 'libcadin.game-screen'
 local splash = require 'libcadin.splash-screen'
 local window = require 'libcadin.window'
 local sprites = require 'components.sprites'
+local Invaders = require 'components.invaders'
 local Spaceship = require 'components.spaceship'
+
+local IMG = love.graphics.newImage 'assets/sprites/sprite_sheet.png'
+
+local invaders_img = sprites.load_invaders(IMG)
+local ship_img = sprites.load_ship(IMG)
+local spaceship = Spaceship:load(ship_img, window.center.x - (sprites.x / 2), game_screen.pos_y1 - sprites.y - 10)
+local invaders_table = Invaders:load_table(invaders_img, game_screen.pos_x0, game_screen.pos_y0)
 
 function love.load()
   love.graphics.setBackgroundColor(catppuccin.MANTLE)
@@ -12,22 +20,18 @@ function love.load()
   local fira_mono = love.graphics.newFont(font_asset_path, 48)
   love.graphics.setFont(fira_mono)
 
-  IMG = love.graphics.newImage 'assets/sprites/sprite_sheet.png'
-
   splash.load()
-
-  invaders = sprites.load_invaders(IMG)
-  ship_img = sprites.load_ship(IMG)
-
-  spaceship = Spaceship:load(ship_img, window.center.x - (sprites.x / 2), game_screen.pos_y1 - sprites.y - 10)
 end
 
 function love.update(dt)
   sprites.update_frame(dt)
+
+  for i = 1, #invaders_table do
+    invaders_table[i]:move(dt)
+  end
+
   spaceship:move('left', 'right')
 end
-
-local margin = (game_screen.width - (11 * (sprites.x + 10))) / 2
 
 function love.draw()
   local time = love.timer.getTime()
@@ -36,12 +40,8 @@ function love.draw()
   if time >= 0 then
     game_screen.frame()
 
-    for i = 1, 11, 1 do
-      love.graphics.draw(IMG, invaders[1][sprites.current_frame], game_screen.pos_x0 + i * sprites.x + margin, game_screen.pos_y0 + sprites.y * 1)
-      love.graphics.draw(IMG, invaders[2][sprites.current_frame], game_screen.pos_x0 + i * sprites.x + margin, game_screen.pos_y0 + sprites.y * 2.3)
-      love.graphics.draw(IMG, invaders[2][sprites.current_frame], game_screen.pos_x0 + i * sprites.x + margin, game_screen.pos_y0 + sprites.y * 3.5)
-      love.graphics.draw(IMG, invaders[3][sprites.current_frame], game_screen.pos_x0 + i * sprites.x + margin, game_screen.pos_y0 + sprites.y * 4.8)
-      love.graphics.draw(IMG, invaders[3][sprites.current_frame], game_screen.pos_x0 + i * sprites.x + margin, game_screen.pos_y0 + sprites.y * 6.1)
+    for i = 1, #invaders_table do
+      invaders_table[i]:draw(IMG, sprites.current_frame)
     end
 
     spaceship:draw(IMG)
