16db514feef5492447f6b7e00cbcaaf8d6cabfdf
lpnh <paniguel.lpnh@gmail.com>
2024-08-28 11:19:39 -0300

    feat: add laser invader collision

--- components/invaders.lua
+++ components/invaders.lua
@@ -5,16 +5,14 @@ local Invaders = {}
 Invaders.__index = Invaders
 
 Invaders.ROW = 0
-Invaders.SPD = 1
+Invaders.SPD = 0.9
 
-local function new_invader(sprite_img, kind, x, y, row)
+local function new_invader(sprite_img, kind, x, y)
   local instance = setmetatable({}, Invaders)
   instance.img = sprite_img
   instance.kind = kind
-  instance.x = x
-  instance.y = y
-  -- instance.speed = 2
-  -- instance.row = row -- not sure about the row logic yet
+  instance.x0 = x
+  instance.y0 = y
   instance.alive = true
   return instance
 end
@@ -32,9 +30,9 @@ function Invaders:load_table(sprite_img, x0, y0)
       kind = 3
     end
     for j = 1, 11 do
-      local x = x0 + (j - 1) * sprites.x + 130
-      local y = y0 + (i - 1) * sprites.y * 1.2
-      local add_invader = new_invader(sprite_img[kind], kind, x, y, i)
+      local x = x0 + (j - 1) * sprites.width + 130
+      local y = y0 + (i - 1) * sprites.height * 1.2
+      local add_invader = new_invader(sprite_img[kind], kind, x, y)
       table.insert(invaders_table, add_invader)
     end
   end
@@ -46,17 +44,25 @@ function Invaders:increase_speed(factor)
   Invaders.SPD = Invaders.SPD * factor
 end
 
-local delta = 0
-
-function Invaders:move(dt)
-  sprites.frame_duration = sprites.frame_duration - dt * 0.0005
-
+function Invaders:move(laser)
   if Invaders.ROW < 16 then
     if self.alive then
-      if self.x < game_screen.pos_x1 - sprites.y - 10 and Invaders.SPD > 0 then
-        self.x = self.x + Invaders.SPD
-      elseif self.x > game_screen.pos_x0 + 5 and Invaders.SPD < 0 then
-        self.x = self.x + Invaders.SPD
+      -- OTHER OPTION FOR LASER X INVADER COLLISION
+      -- if
+      --   laser.x0 + laser.width >= self.x0
+      --   and laser.x0 <= self.x0 + sprites.width
+      --   and laser.y0 <= self.y0 + sprites.height + Invaders.ROW * sprites.height
+      --   and laser.y0 + laser.height >= self.y0 + Invaders.ROW * sprites.height
+      -- then
+      --   laser.x0 = 0
+      --   laser.y0 = 0
+      --   self.alive = false
+      --   laser.cooldown = false
+      --   return
+      -- end
+
+      if (self.x0 < game_screen.pos_x1 - sprites.width - 4 and Invaders.SPD > 0) or (self.x0 > game_screen.pos_x0 + 4 and Invaders.SPD < 0) then
+        self.x0 = self.x0 + Invaders.SPD
       else
         Invaders.ROW = Invaders.ROW + 1
         Invaders.SPD = -Invaders.SPD * 1.1
@@ -69,7 +75,7 @@ end
 
 function Invaders:draw(sprite_img, frame)
   if self.alive then
-    love.graphics.draw(sprite_img, self.img[frame], self.x, self.y + Invaders.ROW * sprites.y)
+    love.graphics.draw(sprite_img, self.img[frame], self.x0, self.y0 + Invaders.ROW * sprites.height)
   end
 end
 
--- components/laser.lua
+++ components/laser.lua
@@ -1,32 +1,57 @@
 local game_screen = require 'libcadin.game-screen'
+local sprites = require 'components.sprites'
 
 local laser = {}
 
 laser.cooldown = false
-laser.x = 0
-laser.y = 0
-laser.speed = 10
+laser.x0 = 0
+laser.y0 = 0
+laser.width = 4
+laser.height = 10
+laser.speed = 11
 
-function laser:move()
+function laser:move(invaders_table)
   if self.cooldown then
-    self.y = self.y - self.speed
-    if laser.y < game_screen.pos_y0 then
-      laser.cooldown = false
+    self.y0 = self.y0 - self.speed
+
+    if self.y0 < game_screen.pos_y0 then
+      self.x0 = 0
+      self.y0 = 0
+      self.cooldown = false
+      return
+    end
+
+    for i = 1, #invaders_table do
+      local inv = invaders_table[i]
+      if inv.alive then
+        if
+          self.x0 + laser.width >= inv.x0
+          and self.x0 <= inv.x0 + sprites.width
+          and self.y0 <= inv.y0 + sprites.height + inv.ROW * sprites.height
+          and self.y0 + laser.height >= inv.y0 + inv.ROW * sprites.height
+        then
+          self.x0 = 0
+          self.y0 = 0
+          inv.alive = false
+          self.cooldown = false
+          break
+        end
+      end
     end
   end
 end
 
 function laser:fire(spaceship)
-  if not laser.cooldown then
-    self.x = spaceship.x + 22.5
-    self.y = spaceship.y - 15
-    laser.cooldown = true
+  if not self.cooldown then
+    self.x0 = spaceship.x + 22.5
+    self.y0 = spaceship.y - 15
+    self.cooldown = true
   end
 end
 
 function laser:draw()
-  if self.y > game_screen.pos_y0 and self.cooldown then
-    love.graphics.rectangle('fill', self.x, self.y, 4, 10)
+  if self.y0 > game_screen.pos_y0 and self.cooldown then
+    love.graphics.rectangle('fill', self.x0, self.y0, self.width, self.height)
   end
 end
 
