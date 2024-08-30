67805ddece9ce0943e7278f21e6fd2651221c2e8
lpnh <paniguel.lpnh@gmail.com>
2024-08-30 12:13:43 -0300

    refactor: update font implementation

--- components/scoreboard.lua
+++ components/scoreboard.lua
@@ -1,9 +1,14 @@
+local fonts = require 'libcadin.fonts'
+local game_screen = require 'libcadin.game-screen'
 local window = require 'libcadin.window'
+
 local score = {}
 
 function score.draw(SCORE)
   local formatted_score = string.format('%05d', SCORE)
-  love.graphics.print('Score: ' .. formatted_score, window.width - 380, window.height - 60)
+  local text = 'Score: ' .. formatted_score
+  local text_width = fonts.ps2p_medium:getWidth(text)
+  love.graphics.printf(text, fonts.ps2p_medium, window.center.x - (text_width / 2), game_screen.pos_y0 - 100, text_width, 'center')
 end
 
 return score
